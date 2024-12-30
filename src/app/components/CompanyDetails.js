1eedc57b69a83b53c0e65edc9302fe20c88ff58e
waseemAnsari11011 <waseemahm11011@gmail.com>
2024-12-30 08:38:01 +0530

    abc

--- src/app/components/CompanyDetails.js
+++ src/app/components/CompanyDetails.js
@@ -18,7 +18,7 @@ const AnimatedNumber = ({ value }) => {
 
 const CompanyDetails = () => {
   const [ref, inView] = useInView({
-    threshold: 0.1,
+    threshold: 0,
   });
   const [bgLoaded, setBgLoaded] = useState(false);
 
