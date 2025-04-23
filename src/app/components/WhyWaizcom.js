8b41482e71ad795aa82b6f91be08ca7396621f1a
waseemAnsari11011 <waseemahm11011@gmail.com>
2025-04-23 18:26:56 +0530

    abc

--- src/app/components/WhyWaizcom.js
+++ src/app/components/WhyWaizcom.js
@@ -43,62 +43,16 @@ const WhyWaizcom = () => {
           </ul>
         </div>
         <div
-          className={`${
+          className={`bg-[#f4f5f6] p-6 md:p-8 shadow rounded-2xl ${
             bgLoaded
               ? "bg-[url(/bg/noise1.png)] w-full mx-auto my-5 relative  bg-cover bg-no-repeat  opacity-100"
               : "opacity-0"
-          } transition-opacity duration-100 bg-[#f4f5f6] p-6 md:p-8 shadow rounded-2xl `}
+          } transition-opacity duration-500`}
         >
           <h3 className="text-4xl font-bold mb-4">02</h3>
-          <h4 className="text-2xl font-semibold mb-2">Design</h4>
-          <ul className="list-disc pl-5 space-y-2">
-            <li>
-              Unique, custom logos in versatile formats (vector, PNG, SVG).
-            </li>
-            <li>
-              Email signatures, business cards, and company profiles for a
-              unified identity.
-            </li>
-            <li>
-              Consistent guidelines for colors, typography, and style to
-              maintain professionalism across all platforms.
-            </li>
-          </ul>
-        </div>
-        <div
-          className={`bg-[#f4f5f6] p-6 md:p-8 shadow rounded-2xl ${
-            bgLoaded
-              ? "bg-[url(/bg/noise1.png)] w-full mx-auto my-5 relative  bg-cover bg-no-repeat  opacity-100"
-              : "opacity-0"
-          } transition-opacity duration-500`}
-        >
-          <h3 className="text-4xl font-bold mb-4">03</h3>
-          <h4 className="text-2xl font-semibold mb-2">Marketing & Branding</h4>
-          <ul className="list-disc pl-5 space-y-2">
-            <li>Targeted campaigns to reach your audience.</li>
-            <li>
-              12 curated images/text posts + 6 high-impact reels to keep your
-              social media fresh.
-            </li>
-            <li>
-              Optimize search rankings and streamline engagement with expert
-              social media management.
-            </li>
-          </ul>
-        </div>
-
-        <div
-          className={`bg-[#f4f5f6] p-6 md:p-8 shadow rounded-2xl ${
-            bgLoaded
-              ? "bg-[url(/bg/noise1.png)] w-full mx-auto my-5 relative  bg-cover bg-no-repeat  opacity-100"
-              : "opacity-0"
-          } transition-opacity duration-500`}
-        >
-          <h3 className="text-4xl font-bold mb-4">04</h3>
           <h4 className="text-2xl font-semibold mb-2">Risk-Free</h4>
           <ul className="list-disc pl-5 space-y-2">
             <li> Cancel anytime before 30 days for a full refund.</li>
-            <li> Your One-Stop-Shop for digital services</li>
           </ul>
         </div>
       </div>
