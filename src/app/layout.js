ab61b28eb875901988abe1d6d737acdb06ea246c
waseemAnsari11011 <waseemahm11011@gmail.com>
2026-01-02 08:57:54 +0530

    2jan

--- src/app/layout.js
+++ src/app/layout.js
@@ -74,6 +74,16 @@ export default function RootLayout({ children }) {
           window.gtag_report_conversion = gtag_report_conversion;
         `}
       </Script>
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "uuyhumnre9");
+        `}
+      </Script>
       <body className={montserrat.className}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
