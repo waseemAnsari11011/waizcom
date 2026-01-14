80527eb4b99344c25b7594839658ed77e7a02cdc
waseemAnsari11011 <waseemahm11011@gmail.com>
2026-01-14 12:33:27 +0530

    14 jan

--- src/app/layout.js
+++ src/app/layout.js
@@ -84,6 +84,22 @@ export default function RootLayout({ children }) {
           })(window, document, "clarity", "script", "uuyhumnre9");
         `}
       </Script>
+      {/* Meta Pixel Code */}
+     <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1444023683753772');
+fbq('track', 'PageView');
+        `}
+      </Script>
+      {/* End Meta Pixel Code */}
       <body className={montserrat.className}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -95,6 +111,17 @@ export default function RootLayout({ children }) {
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1444023683753772&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code (noscript) */}
         <Header />
         <main>{children}</main>
         <WhatsAppButton phoneNumber={"918882202176"} />
